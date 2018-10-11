0afae5003309813e865082c9a26ae2087f75cb66
kihy0804 <kihy0804@gmail.com>
2018-10-11 13:22:30 -0600

    Hoyun's work

---
+++ Data_Structure_Group2/Controllers/StackController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Data_Structure_Group2.Controllers
+{
+    public class StackController : Controller
+    {
+        //static is always there for you to use, alive, once you start the website that myStack is alive
+        //So make your variable static
+        static Stack<string> myStack = new Stack<string>();
+
+        // GET: Stack
+        public ActionResult Index()
+        {
+            ViewBag.StackView = myStack;
+            return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            myStack.Push("New Entry " + (myStack.Count + 1));
+            ViewBag.StackView = myStack;
+            return View("Index");
+        }
+    }
+}
---
+++ Data_Structure_Group2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Data_Structure_Group2.MvcApplication" Language="C#" %>
